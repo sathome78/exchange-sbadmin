7dd17a8dd69bfaa3292089eac730572bb092bd4c
kmelnik <kirill.melnik@upholding.biz>
2018-11-19 16:42:04 +0200

    add source files & srping boot admin app

---
+++ src/main/java/me/exrates/SbAdminApplication.java
@@ -0,0 +1,15 @@
+package me.exrates;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SbAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SbAdminApplication.class, args);
+    }
+}
