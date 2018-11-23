a8d54467b8ee850716ae112be3092d774051d755
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2018-11-23 17:36:50 +0200

    add eureka properties / min fix

--- src/main/java/me/exrates/SbAdminApplication.java
+++ src/main/java/me/exrates/SbAdminApplication.java
@@ -2,12 +2,12 @@ package me.exrates;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class SbAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(SbAdminApplication.class, args);
